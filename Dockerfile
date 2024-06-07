12b4041992831841544207c23fc3d4802a741125
farheenahmed <farheenghori@hotmail.com>
2024-06-07 13:42:56 -0500

    Modified docker files

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM tomcat:latest
+
+# Remove default ROOT webapp
+RUN rm -rf /usr/local/tomcat/webapps/ROOT
+
+# Copy WAR file to Tomcat webapps directory
 COPY target/*.war /usr/local/tomcat/webapps/ROOT.war
+
 EXPOSE 9090
+
 CMD ["catalina.sh", "run"]
--- src/test/java/com/sample/helloworld/controller/HelloWorldControllerTest.java
+++ src/test/java/com/sample/helloworld/controller/HelloWorldControllerTest.java
@@ -1,6 +1,7 @@
 package com.sample.helloworld.controller;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,21 +10,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloWorldControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @Test
     public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello World!!!")));
+                .andExpect(content().string(is("Hello World!!!")));
+
     }
 }
