648eeae941624f543e65906c6201210ea8180ef2
farheenahmed <farheenghori@hotmail.com>
2024-07-24 16:17:32 -0500

    Added terraform to project

---
+++ terraform/ecs-taskdefinition.tf
@@ -0,0 +1,28 @@
+resource "aws_ecs_task_definition" "TD" {
+  family                   = "nginx"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.iam-role.arn
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions = jsonencode([
+    {
+      name      = "main-container"
+      image     = "211125712788.dkr.ecr.us-east-2.amazonaws.com/hello-world:latest"
+      cpu       = 1024
+      memory    = 2048
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
+    }
+  ])
+}
+
+
+data "aws_ecs_task_definition" "TD" {
+  task_definition = aws_ecs_task_definition.TD.family
+}
